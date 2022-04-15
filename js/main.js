af173e02f2754d0aa28a7a46b2c036863d91dfe8
Farah <fn.rusli@outlook.com>
2022-04-15 14:08:09 +0800

    Minify files

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-/**
- * Dropdown menu
- */
-document.addEventListener('DOMContentLoaded', function() {
-    const menus = document.querySelectorAll('.navbar-burger');
-    const dropdowns = document.querySelectorAll('.navbar-menu');
-
-    if (menus.length && dropdowns.length) {
-        for (var i = 0; i < menus.length; i++) {
-            menus[i].addEventListener('click', function() {
-                for (var j = 0; j < dropdowns.length; j++) {
-                    dropdowns[j].classList.toggle('is-active');
-                }
-            });
-        }
-    }
-});
+document.addEventListener("DOMContentLoaded",(function(){const e=document.querySelectorAll(".navbar-burger"),n=document.querySelectorAll(".navbar-menu");if(e.length&&n.length)for(var t=0;t<e.length;t++)e[t].addEventListener("click",(function(){for(var e=0;e<n.length;e++)n[e].classList.toggle("is-active")}))}));
